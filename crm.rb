88f12aa396d66280c02bc9df3e4dbab3dac4146d
David FletcherE <dtsfletcher@gmail.com>
2017-08-23 16:44:03 -0400

    almost done'

--- crm.rb
+++ crm.rb
@@ -6,3 +6,20 @@ require 'sinatra'
 get '/home' do
 erb :index
 end
+
+get '/contacts' do
+erb :contacts
+@contacts = [crm.sqlite3]
+end
+
+get '/about' do
+erb :about
+end
+
+
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
