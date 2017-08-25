b3d7b701f05cb0062cb60708620cb78724501659
David FletcherE <dtsfletcher@gmail.com>
2017-08-25 15:42:50 -0400

    moving on

--- crm.rb
+++ crm.rb
@@ -1,29 +1,34 @@
 require_relative 'contact'
 require 'sinatra'
 
-get '/'do
-@contactspage = '/contacts'
-@aboume = '/about'
-
-  erb :index
-end
-
-get '/home' do
-erb :index
-end
-
-get '/contacts/new' do
-  erb :new
-end
-
-get '/about' do
+  get '/'do
   @contactspage = '/contacts'
-  @home_page = '/'
-erb :about
-end
+  @aboume = '/about'
+
+    erb :index
+  end
+
+  get '/home' do
+  erb :index
+  end
+
+  get '/about' do
+    @contactspage = '/contacts'
+    @home_page = '/'
+  erb :about
+  end
 
 
-get '/contacts' do
+  post '/contacts' do
+    Contact.create(first_name: params[:first_name],last_name: params[:last_name],email: params[:email],note: params[:note])
+    redirect to '/contacts'
+  
+  end
+
+
+
+
+  get'/contacts' do
   @contacts = Contact.all
   @size = Contact.all.size
   @home_page = '/'
@@ -31,24 +36,39 @@ get '/contacts' do
   erb :contacts
 end
 
-get '/contacts/:id' do
-  @home_page = '/'
-  @aboutme = '/about'
-  @contacts = Contact.find_by({id: params[:id].to_i})
-  @contactspage = '/contacts'
-  if @contact
-  erb :show_contact
-else
-    raise Sinatra: :Notfound
+  get '/contacts/new' do
+    erb :new
   end
-end
 
-get '/about' do
-  @contacts_page = '/contacts'
-  @home_page = '/'
-erb :about
 
-end
+
+
+
+
+  get '/contacts/:id' do
+    @home_page = '/'
+    @aboutme = '/about'
+    @contacts = Contact.find_by({id: params[:id].to_i})
+    @contactspage = '/contacts'
+    if @contact
+    erb :show_contact
+  else
+      raise Sinatra: :Notfound
+    end
+  end
+
+  get '/contacts' do
+    puts params
+  end
+
+
+
+  # get '/about' do
+  #   @contacts_page = '/contacts'
+  #   @home_page = '/'
+  # erb :about
+  # end
+
 
 
 after do
