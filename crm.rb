ad2f78356d444e45ee3c66fcf4ef8088025ed603
David FletcherE <dtsfletcher@gmail.com>
2017-08-25 16:46:16 -0400

    i'm done

--- crm.rb
+++ crm.rb
@@ -69,13 +69,37 @@ end
     else
     raise Sinatra::NotFound
   end
+end
 
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
 
-
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
   end
+end
 
 
 
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
