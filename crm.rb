b8220ffa34c0eaf3ad4bf3d7c6622ec29ebb3904
David FletcherE <dtsfletcher@gmail.com>
2017-08-24 21:13:11 -0400

    NO IDEA

--- crm.rb
+++ crm.rb
@@ -2,6 +2,9 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/'do
+@contactspage = '/contacts'
+@aboume = '/about'
+
   erb :index
 end
 
@@ -9,12 +12,29 @@ get '/home' do
 erb :index
 end
 
-get '/contact' do
-erb :contacts
-# @contacts = Contact.all
+get '/contacts' do
+  @contacts = Contact.all
+  @size = Contact.all.size
+  @home_page = '/'
+  @aboutme = '/about'
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  @home_page = '/'
+  @aboutme = '/about'
+  @contacts = Contact.find_by({id: params[:id].to_i})
+  @contactspage = '/contacts'
+  if @contact
+  erb :show_contact
+else
+    raise Sinatra: :Notfound
+  end
 end
 
 get '/about' do
+  @contacts_page = '/contacts'
+  @home_page = '/'
 erb :about
 
 end
